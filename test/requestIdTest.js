bc30372df01fe44ec8f07a5fcf170f9e402395cf
eliran zach <thehobbit85@gmail.com>
2015-10-25 16:48:35 +0200

    Got request ID test to 100% coverage

--- test/requestIdTest.js
+++ test/requestIdTest.js
@@ -1,5 +1,6 @@
+/* global describe, it */
+
 var casimircore = require('..')()
-var requestid = casimircore.request_id({secret: '1234', namespace: 'server1'})
 var request = require('supertest')
 var express = require('express')
 var assert = require('assert')
@@ -8,15 +9,20 @@ var rid = 'server1-7d3f17a1-f94c-46b0-a69f-b54a71f8415d'
 var fakeRid = '123'
 var sid = '8f79a94cbe09bdb4ce283e9a67c518c5'
 var wrongSid = '8f78a94cbe09bdb4ce283e9a67c518c5'
-var app = express()
-
-app.use(requestid)
-app.get('/', function (req, res, next) {
-  return res.send(200)
-})
-app.use(casimircore.error('development'))
 
 describe('Checking requestid', function () {
+  var requestid = casimircore.request_id({secret: '1234', namespace: 'server1'})
+  var app = express()
+
+  app.use(requestid)
+  app.get('/', function (req, res, next) {
+    return res.sendStatus(200)
+  })
+  app.use(casimircore.error('development'))
+  it('should throw error since secret is mandatory', function (done) {
+    assert.throws(function () { casimircore.request_id({ namespace: 'server1' }) }, 'Must provide secret')
+    done()
+  })
   it('should return 200 with a request ID in the response header', function (done) {
     request(app)
     .get('/')
@@ -59,5 +65,38 @@ describe('Checking requestid', function () {
     .set('request-id', rid)
     .set('service-secret', wrongSid)
     .expect(401, done)
+  })
+})
+
+describe('Checking requestid with default name', function () {
+  var requestid = casimircore.request_id({secret: '1234'})
+  var app = express()
+  app.use(requestid)
+  app.get('/', function (req, res, next) {
+    return res.sendStatus(200)
+  })
+  app.use(casimircore.error('development'))
+  it('should return 200 with default namespace', function (done) {
+    request(app)
+    .get('/')
+    .expect(function (res) {
+      assert.equal(res.headers['request-id'].split('-')[0], 'namespace', 'Namespace should be namespace')
+    })
+    .expect(200, done)
+  })
+})
+
+describe('Checking requestid throws', function () {
+  var requestid = casimircore.request_id({secret: 123})
+  var app = express()
+  app.use(requestid)
+  app.get('/', function (req, res, next) {
+    return res.sendStatus(200)
+  })
+  app.use(casimircore.error('development'))
+  it('should return 500 since secret is not a buffer or string', function (done) {
+    request(app)
+    .get('/')
+    .expect(500, done)
   })
 })
